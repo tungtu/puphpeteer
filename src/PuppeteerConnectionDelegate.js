94b9cdd3417e802ee9fdfadf1f87c646edd842e0
tungtt1 <tungtt1@coccoc.com>
2025-05-15 11:32:33 +0700

    fix dependencies

--- src/PuppeteerConnectionDelegate.js
+++ src/PuppeteerConnectionDelegate.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const puppeteer = require('puppeteer'),
-    {ConnectionDelegate} = require('@nesk/rialto'),
-    Logger = require('@nesk/rialto/src/node-process/Logger'),
-    ConsoleInterceptor = require('@nesk/rialto/src/node-process/NodeInterceptors/ConsoleInterceptor'),
-    StandardStreamsInterceptor = require('@nesk/rialto/src/node-process/NodeInterceptors/StandardStreamsInterceptor');
+    {ConnectionDelegate} = require('@tungtu/rialto'),
+    Logger = require('@tungtu/rialto/src/node-process/Logger'),
+    ConsoleInterceptor = require('@tungtu/rialto/src/node-process/NodeInterceptors/ConsoleInterceptor'),
+    StandardStreamsInterceptor = require('@tungtu/rialto/src/node-process/NodeInterceptors/StandardStreamsInterceptor');
 
 /**
  * Handle the requests of a connection to control Puppeteer.
